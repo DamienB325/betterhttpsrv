3f4a80a6519c04b244531cd710b884591e8e8923
DamienB325 <damien.brown15@outlook.com>
2023-03-24 10:54:25 -0700

    BetterBread2

--- src/StaticServer.ts
+++ src/StaticServer.ts
@@ -2,7 +2,7 @@ import {Server} from "./Server";
 import * as path from "path";
 import * as net from "net";
 import * as fs from "fs";
-
+import * as mime from "mime";
 export class StaticServer extends Server {
     staticFolderPath:string = "none";
     constructor(floderpath: string = path.join(__dirname, "/public")) {
@@ -16,17 +16,29 @@ export class StaticServer extends Server {
             let fpathsplit = fpath.split("/");
             let bufout: Buffer = new Buffer("");
             let restxt:string = "";
+            let tpath:string = "";
+            let res:string = "";
 
             try {
                 if(fpathsplit[fpathsplit.length].includes(".")) {
-                    bufout = fs.readFileSync(path.join(staticfloderpath, fpath));
+                    tpath = path.join(staticfloderpath, fpath);
                 } else {
-
+                    tpath = path.join(staticfloderpath, fpath+".html");
                 }
-            } catch(e) {
+                bufout = fs.readFileSync(tpath);
+                restxt = bufout.toString();
+                res = `HTTP/1.1 200 OK
+Content-Type: ${mime.getType(tpath)}
+Content-Length: ${restxt.length}
 
+
+${restxt}`;
+            } catch(e) {
+                restxt = ``;
+                res = `HTTP/1.1 404 NOT FOUND
+`;
             }
-            srv.send(bufout.toString())
+            srv.write(res)
         });
     }
 }
